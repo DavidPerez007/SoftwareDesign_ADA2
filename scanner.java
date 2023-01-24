ec084a02b1afb5aa6da115f309b9a88192e3c89d
david <davipcleo@gmail.com>
2023-01-24 16:31:54 -0600

    Deleting unused file scanner.java

--- scanner.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class scanner {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int a = scan.nextInt();
-        System.out.println("valor:" + a);
-    }
-}
